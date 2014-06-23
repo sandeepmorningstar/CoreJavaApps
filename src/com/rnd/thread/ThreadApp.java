dbe68e389f87acbf1431fa12c0baea294990f772
sandeepmgabhale <sandeepgabhale@gmail.com>
2014-06-23 17:53:10 +0530

    Create ThreadApp.java

---
+++ src/com/rnd/thread/ThreadApp.java
@@ -0,0 +1,11 @@
+public class ThreadApp implements Runnable {
+  
+  public static void main(String[] args) {
+  
+  }
+  
+  @Override
+  public void run() {
+    System.out.println("\n\tThread executed");
+  }
+}
